1fc72adfb332e7e2e89ef00e0225dde3c7f8a49a
Chayma <chayma@example.com>
2026-05-11 10:22:29 +0200

    Changer l'onglet douche par activité

--- src/data/mockPlanning.js
+++ src/data/mockPlanning.js
@@ -1,9 +1,33 @@
 export const DUREE_DOUCHE = 20
 
 export const mockPlanningSemaine19 = {
-  2: { lundi: 'SE2', mardi: null, mercredi: null, jeudi: null, vendredi: null, samedi: null, dimanche: null },
-  3: { lundi: null, mardi: null, mercredi: null, jeudi: null, vendredi: null, samedi: null, dimanche: null },
-  12: { lundi: null, mardi: null, mercredi: null, jeudi: null, vendredi: null, samedi: null, dimanche: null }
+  2: { 
+    lundi: { as: 'SE2', activity: 'douche' }, 
+    mardi: { as: 'SE1', activity: 'wc' }, 
+    mercredi: { as: 'SC1', activity: 'toilette' }, 
+    jeudi: { as: 'SE2', activity: 'coucher' }, 
+    vendredi: { as: 'SC2', activity: 'repas' }, 
+    samedi: { as: null, activity: null }, 
+    dimanche: { as: null, activity: null } 
+  },
+  3: { 
+    lundi: { as: null, activity: null }, 
+    mardi: { as: 'SE2', activity: 'wc' }, 
+    mercredi: { as: null, activity: null }, 
+    jeudi: { as: null, activity: null }, 
+    vendredi: { as: null, activity: null }, 
+    samedi: { as: null, activity: null }, 
+    dimanche: { as: null, activity: null } 
+  },
+  12: { 
+    lundi: { as: null, activity: null }, 
+    mardi: { as: null, activity: null }, 
+    mercredi: { as: 'SC1', activity: 'repas' }, 
+    jeudi: { as: null, activity: null }, 
+    vendredi: { as: null, activity: null }, 
+    samedi: { as: null, activity: null }, 
+    dimanche: { as: null, activity: null } 
+  }
 }
 
 export const joursSemaine = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
@@ -11,7 +35,7 @@ export const joursSemaine = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 
 export function createEmptyPlanningForPatients(patients) {
   return patients.reduce((planning, patient) => {
     planning[patient.id] = joursSemaine.reduce((jours, jour) => {
-      jours[jour] = null
+      jours[jour] = { as: null, activity: null }
       return jours
     }, {})
     return planning
